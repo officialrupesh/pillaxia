e847cedd85946c3650e024e5469ce45562d5d61b
Rupesh Raut <iamrupeshraut1@gmail.com>
2024-07-03 18:55:53 +0545

    fix: :art: HeroSection animation updates

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
         <div className="container">
           <section className="hero-section-container row">
             <div className="col-12 col-lg-8">
-              <ScrollAnimation animatePreScroll={false} animateIn="slideInDown" duration={1} className="d-flex">
+              <ScrollAnimation className="d-flex">
                 <div className="path-bg d-flex align-items-center">
                   <div className="dot-before"></div>
                   <h6>Your Path to better health</h6>
@@ -26,14 +26,14 @@ const HeroSection = () => {
                   Seamlessly manage medications with confidence, through enhanced daily adherence and fostering collaboration among patients, caregivers, and healthcare providers. Experience a healthier, more connected care journey with Pillaxia.
                 </p>
               </ScrollAnimation>
-              <ScrollAnimation animatePreScroll={false}  animateIn="bounceInLeft" duration={2} delay={3}>
+              <ScrollAnimation animatePreScroll={false}  animateIn="bounceInLeft" duration={1} delay={3}>
                 <a href="#">
                   <button className="btn btn-join" style={{ marginBottom: "80px", minWidth: "260px", minHeight: "56px" }}>
                     Get Started
                   </button>
                 </a>
               </ScrollAnimation>
-              <div>
+              <ScrollAnimation animatePreScroll={false} animateIn="fadeIn" duration={2} delay={3}>
                 <div className="associated-programs-wrapper">
                 <h6 className="fw-medium">Pillaxia has participated in a number of programmes and accelerators</h6>
                   <div className="associated-programs d-flex flex-wrap gap-3 justify-content-center justify-cotent-md-start">
@@ -49,16 +49,16 @@ const HeroSection = () => {
 
                  </div>
                 </div>
-              </div>
+              </ScrollAnimation>
 
             </div>
-            <div className="col-12 col-lg-4 d-lg-block d-none">
+            <ScrollAnimation animatePreScroll={false} animateIn="bounceInRight" duration={2}  className="col-12 col-lg-4 d-lg-block d-none">
               <div className="hero-img d-flex justify-content-center align-items-center">
                 <Image className="img-fluid" src={"/images/hero-img.png"} alt="Hero section image" height={625} width={340}/>
 
               </div>
 
-            </div>
+            </ScrollAnimation>
           </section>
 
         </div>
